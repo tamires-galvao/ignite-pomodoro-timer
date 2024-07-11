6066073f4e60e0234f3897036a720943624fc12b
tamires moreno <tamiresmorenogalvao@gmail.com>
2024-07-10 21:08:43 -0300

    Ajustando cores e fontes

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -20,12 +20,6 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border: 0;
   margin: 8px;
 
-  background-color: ${props => props.theme.primary};
+  background-color: ${props => props.theme['green-500']};
   color: ${props => props.theme.white};
-
-  /* ${props => { 
-    return css`
-      background-color: ${buttonVariants[props.variant]}
-      `
-  }} */
   `;
